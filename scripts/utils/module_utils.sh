salvogiangri/UN1CA

unica: patches: fmradio: init
Change-Id: Ice45aaca5c159669a83b02afe0f463246e7e39e0

scripts/utils/module_utils.sh
@@ -76,7 +76,8 @@ GET_GALAXY_STORE_DOWNLOAD_URL()
 
     # Galaxy S25 Ultra EUR_OPENX
     # Galaxy S22 Ultra GBL_OPENX
-    DEVICES=("SM-S938B" "SM-S901E")
+    # Galaxy S25 Ultra KOR_SINGLEX
+    DEVICES=("SM-S938B" "SM-S901E" "SM-S938N")
 
     OS="$(GET_PROP "system" "ro.build.version.sdk")"
     ONEUI="$(GET_PROP "system" "ro.build.version.oneui")"

cosinemyan/NPLROM_UN1CA

(cherry picked from commit 553e341349044cc0a6529a69708d4c2009e3d6c5)

scripts/utils/module_utils.sh
@@ -79,8 +79,9 @@ GET_GALAXY_STORE_DOWNLOAD_URL()
     local PROTOCOL
 
     # Galaxy S26 Ultra EUR_OPENX
+    # Galaxy S25 Ultra EUR_OPENX / KOR_SINGLEX
     # Galaxy S23 Ultra GBL_OPENX
-    DEVICES=("SM-S948B" "SM-S938B")
+    DEVICES=("SM-S948B" "SM-S938B" "SM-S938N")
 
     OS="$(GET_PROP "system" "ro.build.version.sdk")"
     ONEUI="$(GET_PROP "system" "ro.build.version.oneui")"